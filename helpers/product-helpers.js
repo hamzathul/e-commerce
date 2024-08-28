f7d8058af0b1e777720f870035b8dcba5dc8a51c
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-08-28 08:59:28 +0530

    Issue with database image update

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -2,7 +2,8 @@ var db = require('../config/connection')
 var collection = require('../config/collections');
 const { resolve, reject } = require('promise');
 var objectId = require('mongodb').ObjectId
-const fs = require('fs')
+const fs = require('fs');
+const { response } = require('../app');
 
 
 module.exports = {
@@ -36,5 +37,28 @@ module.exports = {
                 resolve(response)
             })
         })
+    },
+
+    getProductDetails:(proId)=>{
+        return new Promise((resolve, reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id: new objectId(proId)}).then((product)=>{
+                resolve(product)
+            })
+        })
+    },
+    updateProduct:(proId, proDetails)=>{
+        return new Promise((resolve, reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({_id:new objectId(proId)},{
+                $set:{
+                    name: proDetails.name,
+                    description: proDetails.description,
+                    price: proDetails.price,
+                    category: proDetails.category
+                }
+            }).then((response)=>{
+                resolve()
+            })
+        })
     }
+
 };
--- routes/admin.js
+++ routes/admin.js
@@ -43,4 +43,16 @@ router.get('/delete-product/:id',(req, res)=>{
   })
 })
 
+router.get('/edit-product/:id',async (req, res)=>{
+  let product = await productHelpers.getProductDetails(req.params.id)
+  console.log(product)
+  res.render('admin/edit-product',{product})
+})
+
+router.post('/edit-product/:id',(req, res)=>{
+  productHelpers.updateProduct(req.params.id, req.body).then(()=>{
+    res.redirect('/admin')
+  })
+})
+
 module.exports = router;
