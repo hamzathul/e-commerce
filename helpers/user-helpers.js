91b041766537072bdeb30f92e5ae8a820c07fd82
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-08-30 10:49:30 +0530

    adding new changes

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -101,6 +101,13 @@ module.exports = {
                         foreignField:'_id',
                         as:'product'
                     }
+                },
+                {
+                    $project:{
+                        item:1,
+                        quantity:1,
+                        product:{$arrayElemAt:['$product', 0]}
+                    }
                 }
             ]).toArray()
             resolve(cartItems)
